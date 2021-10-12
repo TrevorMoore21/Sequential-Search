b0fe48f994a1be4407976ee1e90831ba7f088979
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-10-12 10:50:13 -0600

    start

--- Sequential Search/src/ImportFile.java
+++ Sequential Search/src/ImportFile.java
@@ -1,12 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class ImportFile
 	{
 
-		public static void main(String[] args)
+		public static void exploreFile() throws IOException
 			{
+				Scanner myFile = new Scanner(new File("FindLetterV.txt"));
 				
-				
-				
+				while (myFile.hasNext())
+					{
+						String words = myFile.nextLine();
+					}
 
 			}
 
