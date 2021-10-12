d279216e445330d62b117438d456ab0f457d9cb8
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-10-12 11:10:19 -0600

    yuh

--- Sequential Search/src/ImportText.java
+++ Sequential Search/src/ImportText.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class ImportText
 	{
 
+		public static void main(String[] args) throws IOException
+			{
+				importDis();
+				
+			}
+		
+		public static void importDis() throws IOException
+		{
+			
+		}
+		
+		
 	}
